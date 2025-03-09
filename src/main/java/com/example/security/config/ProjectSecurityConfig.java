7b0d57f5f5d23ca9916a09a7117b70d4b20ac640
fhafshin <fhafshin@gmail.com>
2025-03-09 11:25:52 +0330

    create database

--- src/main/java/com/example/security/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/security/config/ProjectSecurityConfig.java
@@ -3,11 +3,7 @@ package com.example.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -34,21 +30,34 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    UserDetailsService inMemoryUserDetails() {
-        UserDetails user = User.builder()
-                .username("user").password(passwordEncoder().encode("123"))
-                .authorities("read").build();
 
-        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("123"))
-                .authorities("admin").build();
+    //بارگزاری از روی حافظه
+//    @Bean
+//    UserDetailsService inMemoryUserDetails() {
+//        UserDetails user = User.builder()
+//                .username("user").password(passwordEncoder().encode("123"))
+//                .authorities("read").build();
+//
+//        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("123"))
+//                .authorities("admin").build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+
+
+    //اجرای پیش فرض لاگین
+    //لازم است ابتدا دو جدول مطابق باا جداول پیشنهادی ساخته شود تا کار کند همچنین لازم است پسورد انکدر نیز از نوع عمومی باشد
+//    @Bean
+//    UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new CustomEncodePassword();
+       // return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+       return new CustomEncodePassword();
     }
 
 //استعلام از api پسورد های ساده
--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -2,7 +2,14 @@ create table users(username varchar(50) not null primary key,password varchar(50
 create table authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
 create unique index ix_auth_username on authorities (username,authority);
 
+Insert INTO users VALUES('user','{noop}123',true)
+    Insert INTO users VALUES('admin','{bcrypt}$2a$12$7jKwyOzzRm7mZj1BCOZPeO1Fo5dSXBBB67YxijJYJX12vBPVPKy6y',true)
 
+
+
+
+Insert INTO users VALUES('user','{noop}123',true)
+Insert INTO users VALUES('admin','{bcrypt}$2a$12$7jKwyOzzRm7mZj1BCOZPeO1Fo5dSXBBB67YxijJYJX12vBPVPKy6y',true)
 Insert INTO authorities VALUES('user','read')
-
 Insert INTO authorities VALUES('admin','admin')
+
